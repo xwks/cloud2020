aa73f00bb0eaebdeadea34431d7f49659823af0f
machao <1764561987@qq.com>
2022-06-29 16:08:06 +0800

    third commit

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/StreamMQMain8801.java
@@ -0,0 +1,19 @@
+
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+
+@SpringBootApplication
+public class StreamMQMain8801
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StreamMQMain8801.class,args);
+    }
+}
+
+
+
